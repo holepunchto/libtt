66ab5aeb9d5e40a97a00bd36754becbd5e1e5750
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-10-31 12:57:36 +0100

    Don't read in resize test

--- test/resize.c
+++ test/resize.c
@@ -9,23 +9,6 @@
 uv_loop_t *loop;
 
 static void
-on_read (tt_pty_t *pty, ssize_t read_len, const uv_buf_t *buf) {
-  printf("read\n");
-  if (read_len == UV_EOF) return;
-
-  assert(read_len > 0);
-  printf("%.*s", (int) read_len, buf->base);
-
-  if (buf->base) free(buf->base);
-}
-
-static void
-on_alloc (tt_pty_t *pty, size_t suggested_size, uv_buf_t *buf) {
-  buf->base = malloc(suggested_size);
-  buf->len = suggested_size;
-}
-
-static void
 on_process_exit (tt_pty_t *handle, int64_t exit_status, int term_signal) {
   tt_pty_close(handle, NULL);
 }
@@ -48,9 +31,6 @@ main () {
 
   tt_pty_t pty;
   e = tt_pty_spawn(loop, &pty, &term, &process, on_process_exit);
-  assert(e == 0);
-
-  e = tt_pty_read_start(&pty, on_alloc, on_read);
   assert(e == 0);
 
   e = tt_pty_resize(&pty, 120, 90);
