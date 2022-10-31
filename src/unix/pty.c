8a341ae7d6be84d6119eb97b87892ba4f8b906a0
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-10-31 12:57:46 +0100

    Send `SIGWINCH` to process on resize

--- src/unix/pty.c
+++ src/unix/pty.c
@@ -168,6 +168,10 @@ tt_pty_resize (tt_pty_t *handle, int width, int height) {
 
   if (res < 0) return uv_translate_sys_error(errno);
 
+  res = kill(handle->pid, SIGWINCH);
+
+  if (res < 0) return uv_translate_sys_error(errno);
+
   return 0;
 }
 
